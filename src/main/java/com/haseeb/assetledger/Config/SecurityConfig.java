83497bb0a1e3c98467dd609c917f05668a01566c
Haseeb <92451652+haseebdanish@users.noreply.github.com>
2026-07-09 23:55:22 +0530

    Security updates

--- src/main/java/com/haseeb/assetledger/Config/SecurityConfig.java
+++ src/main/java/com/haseeb/assetledger/Config/SecurityConfig.java
@@ -1,14 +1,25 @@
 package com.haseeb.assetledger.Config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 public class SecurityConfig {
 
     @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/users").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
     }
 
 }
