2c39e7689093f45c92357c40e680b774c2560e5b
Haseeb <92451652+haseebdanish@users.noreply.github.com>
2026-07-12 23:57:04 +0530

    Security Impl

--- src/main/java/com/haseeb/assetledger/Config/SecurityConfig.java
+++ src/main/java/com/haseeb/assetledger/Config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.haseeb.assetledger.Config;
 import com.haseeb.assetledger.Security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -48,5 +50,13 @@ public class SecurityConfig {
         provider.setPasswordEncoder(passwordEncoder());
 
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration)
+            throws Exception {
+
+        return configuration.getAuthenticationManager();
     }
 }
---
+++ src/main/java/com/haseeb/assetledger/Security/JwtService.java
@@ -0,0 +1,13 @@
+package com.haseeb.assetledger.Security;
+
+import com.haseeb.assetledger.Model.User;
+
+public class JwtService {
+
+    void generateToken(User user){
+
+    }
+    void extractUsername(String token) {
+
+    }
+}
